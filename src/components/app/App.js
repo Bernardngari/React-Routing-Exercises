8e4da14d3723033169ccd99c151ee94b8293c433
Bernardngari <bernardngari.ke@gmail.com>
2022-07-20 13:10:01 +0300

    Morning code along session

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,8 @@ import Navbar from '../navbar/Navbar';
 import Contact from '../contact/Contact';
 import About from '../about/About';
 import Home from '../home/Home';
+import {BrowserRouter as Router, Routes,Route, NavLink} from "react-router-dom";
+
 
 function App() {
   return (
@@ -15,6 +17,28 @@ function App() {
   );
 }
 
+function MainApp(){
+return(
+  <Router>
+        <div className="topNavBar">
+            <NavLink className="" to="/">Home</NavLink>.
+            <NavLink to="/contact">Contact</NavLink>
+            <NavLink to="/about">About</NavLink>
+        </div>
+<Routes>
+<Route path='/' element={<Home />}/>
+<Route path='/contact' element={<Contact />}/>
+<Route />
+</Routes>
+
+
+  </Router>
+
+)
+
+}
+
 
 
 export default App;
+export {MainApp};
